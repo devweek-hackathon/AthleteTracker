fb32ba0fb4db58962a7d3beaa7d98972426559eb
Zach Rachlin <zachrach@gmail.com>
2019-02-23 21:59:37 -0800

    tomtom geofencing route is now at tomtom/racerUpdate

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/racers', require('./racers'))
 router.use('/races', require('./races'))
-router.use('/tom', require('./tomtom'))
+router.use('/tomtom', require('./tomtom'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
